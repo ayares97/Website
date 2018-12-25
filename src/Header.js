539ead3bdc3b798ba3192adec4bbf9684ee9e985
Alex Yares <alex@yares.net>
2018-12-24 20:15:10 -0700

    Housekeeping on old files

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component{
+    render(){
+        return(
+            <p>Hello world!</p>
+        );
+    }
+}
+
+export default Header;
