f5d317d8217300ce5360377b1a1c9d6b7c36e8b0
Alex Yares <alex@yares.net>
2019-01-26 14:42:34 -0700

    Fixed issue of images not displaying.

--- src/Head.js
+++ src/Head.js
@@ -4,22 +4,27 @@ import {Container, Jumbotron} from 'reactstrap';
 class Head extends Component{
     render(){
         return(
+
             <div>
                 <Container>
                     <Jumbotron>
                         <h1 className="display-3"> Alex Yares</h1>
                         <row>
                             <a href="https://www.github.com/ayares97">
-                                <img src="Images/github.png" alt="Github"/>
+                                <img src={require("./Images/github.png")} alt="Github"/>
                             </a>
-                            <img src= "./Images/linkedin.png" alt="Linkedin"/>
-                            <img src= "facebook.png" alt="Facebook"/>
-
+                            <a href="https://www.linkedin.com/in/alex-yares-9ab052117/">
+                                <img src={require("./Images/linkedin.png")} alt="Linkedin"/>
+                            </a>
+                            <a href="https://www.facebook.com/alex.yares">
+                                <img src={require('./Images/facebook.png')} alt="Facebook"/>
+                            </a>
                         </row>
                         <hr className="my-2"/>
                     </Jumbotron>
                 </Container>
             </div>
+
         );
     }
 }
