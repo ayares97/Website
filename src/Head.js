e6dcdeefcf22c30b44c10074bd0d2008100d1d77
Alex Yares <alex@yares.net>
2019-01-26 14:57:51 -0700

    Adjusted image size & removed vulns from links.

--- src/Head.js
+++ src/Head.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Container, Jumbotron} from 'reactstrap';
+import './Stylesheets/Head.css'
 
 class Head extends Component{
     render(){
@@ -10,14 +11,15 @@ class Head extends Component{
                     <Jumbotron>
                         <h1 className="display-3"> Alex Yares</h1>
                         <row>
-                            <a href="https://www.github.com/ayares97">
-                                <img src={require("./Images/github.png")} alt="Github"/>
+                            <a href="https://www.github.com/ayares97" target="_blank" rel="noopener noreferrer">
+                                <img className="image" src={require("./Images/github.png")} alt="Github"/>
                             </a>
-                            <a href="https://www.linkedin.com/in/alex-yares-9ab052117/">
-                                <img src={require("./Images/linkedin.png")} alt="Linkedin"/>
+                            <a href="https://www.linkedin.com/in/alex-yares-9ab052117/" target="_blank"
+                               rel="noopener noreferrer">
+                                <img className='image' src={require("./Images/linkedin.png")} alt="Linkedin"/>
                             </a>
-                            <a href="https://www.facebook.com/alex.yares">
-                                <img src={require('./Images/facebook.png')} alt="Facebook"/>
+                            <a href="https://www.facebook.com/alex.yares" target="_blank" rel="noopener noreferrer">
+                                <img className='image' src={require('./Images/facebook.png')} alt="Facebook"/>
                             </a>
                         </row>
                         <hr className="my-2"/>
