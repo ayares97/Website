294f6e49fe87500a9309d2f787304fb1e12a3ac9
Alex Yares <alex@yares.net>
2019-01-28 14:31:45 -0700

    Adjusted index.js to incoporate Bootstrap layouts.

--- src/Head.js
+++ src/Head.js
@@ -1,32 +1,36 @@
-import React, {Component} from 'react';
-import {Container, Jumbotron} from 'reactstrap';
+import React from 'react';
+import {Jumbotron, Row, Container, Col} from 'reactstrap';
 import './Stylesheets/Head.css'
 
-class Head extends Component{
+class Head extends React.Component{
     render(){
         return(
+            <Container>
 
-            <div>
-                <Container>
-                    <Jumbotron>
-                        <h1 className="display-3"> Alex Yares</h1>
-                        <row>
-                            <a href="https://www.github.com/ayares97" target="_blank" rel="noopener noreferrer">
-                                <img className="image" src={require("./Images/github.png")} alt="Github"/>
-                            </a>
-                            <a href="https://www.linkedin.com/in/alex-yares-9ab052117/" target="_blank"
-                               rel="noopener noreferrer">
-                                <img className='image' src={require("./Images/linkedin.png")} alt="Linkedin"/>
-                            </a>
-                            <a href="https://www.facebook.com/alex.yares" target="_blank" rel="noopener noreferrer">
-                                <img className='image' src={require('./Images/facebook.png')} alt="Facebook"/>
-                            </a>
-                        </row>
-                        <hr className="my-2"/>
-                    </Jumbotron>
-                </Container>
-            </div>
-
+                    <Row>
+                        <Col>
+                            <Jumbotron>
+                                <h1 className="display-3"> Alex Yares</h1>
+                                <row>
+                                    <a href="https://www.github.com/ayares97" target="_blank" rel="noopener noreferrer">
+                                        <img className="image" src={require("./Images/github.png")} alt="Github"/>
+                                    </a>
+                                    <a href="https://www.linkedin.com/in/alex-yares-9ab052117/" target="_blank"
+                                       rel="noopener noreferrer">
+                                        <img className='image' src={require("./Images/linkedin.png")} alt="Linkedin"/>
+                                    </a>
+                                    <a href="https://www.facebook.com/alex.yares" target="_blank" rel="noopener noreferrer">
+                                        <img className='image' src={require('./Images/facebook.png')} alt="Facebook"/>
+                                    </a>
+                                </row>
+                                <hr className="my-2"/>
+                            </Jumbotron>
+                        </Col>
+                        <Col>
+                            <p>  </p>
+                        </Col>
+                    </Row>
+            </Container>
         );
     }
 }
